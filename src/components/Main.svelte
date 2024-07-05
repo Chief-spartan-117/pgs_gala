59879d519e48b05469104ab7d60355d6935161b4
Chief-spartan-117 <shresthasagar861@gmail.com>
2024-07-06 00:21:26 +0545

    Responsive Home page

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -45,41 +45,50 @@
     class="fixed left-1/2 top-1/2 flex h-full w-full -translate-x-1/2 -translate-y-[55%] flex-col items-center justify-center text-white max-sm:-translate-y-[60%]"
   >
     <!-- svelte-ignore a11y-missing-attribute -->
-    <img class="-ml-2 mt-8 sm:h-[30rem] max-sm:mt-24 max-sm:w-72" src={mask} />
+    <img
+      class="-ml-2 mt-8 lg:w-96 xl:w-[27rem] max-lg:w-96 max-sm:mt-24 max-sm:w-72"
+      src={mask}
+    />
     <div
-      class="-mb-5 -mt-4 inline-block bg-gradient-to-r from-yellow-600 via-yellow-500 to-yellow-400 bg-clip-text text-center text-3xl font-extrabold text-transparent max-sm:-mt-1 max-sm:text-xl"
+      class="xl:text:3xl -mb-5 -mt-4 inline-block bg-gradient-to-r from-yellow-600 via-yellow-500 to-yellow-400 bg-clip-text text-center text-3xl font-extrabold text-transparent lg:text-2xl max-lg:text-2xl max-sm:-mt-1 max-sm:text-xl"
     >
       Presidential
     </div>
     <div class="mt-3">
-      <img src={galaText} alt="Gala Text" class="w-[24rem] max-sm:w-60" />
+      <img
+        src={galaText}
+        alt="Gala Text"
+        class="w-[24rem] lg:w-80 xl:w-96 max-lg:w-80 max-sm:w-60"
+      />
     </div>
   </div>
 
   <div
-    class="fixed bottom-0 flex w-full -translate-y-8 flex-row items-end justify-between px-16 max-sm:-translate-y-4 max-sm:flex-col max-sm:items-center max-sm:gap-2 max-sm:px-4"
+    class="fixed bottom-0 flex w-full -translate-y-8 flex-row items-end justify-between px-16 max-md:-translate-y-4 max-md:px-4 max-sm:flex-col max-sm:items-center max-sm:gap-2"
   >
-    <div class="max-sm:flex max-sm:flex-row max-sm:gap-2">
-      <p class="font-[cadiz] text-6xl font-black text-white max-sm:text-4xl">
+    <div class="max-md:flex max-md:flex-row max-md:gap-2">
+      <p class="font-[cadiz] text-6xl font-black text-white max-md:text-4xl">
         03
       </p>
-      <p class="font-[cadiz] text-4xl font-black text-white max-sm:text-4xl">
+      <p class="font-[cadiz] text-4xl font-black text-white max-md:text-4xl">
         AUG
       </p>
     </div>
     <div class="flex flex-row items-center gap-4">
-      <p class="text-white max-sm:text-[0.6rem]">Designed & Developed By:</p>
+      <p class="text-white max-md:text-sm max-sm:text-[0.6rem]">
+        Designed & Developed By:
+      </p>
       <div class="flex flex-row items-center">
         {#each devs as dev, index (dev.id)}
           <div
-            class="relative -mr-4 rounded-full border-[0.2rem] border-[rgb(5_16_58)] max-sm:-mr-3"
+            class="relative -mr-4 rounded-full max-md:-mr-3"
             use:tippy={{ content: dev.name, placement: "top" }}
           >
             <img
               src={dev.img}
               alt={dev.name}
-              class="relative w-9 rounded-full max-sm:w-7 z-[{devs.length -
-                index}]"
+              class="relative w-9 rounded-full border-[0.2rem] border-[rgb(5_16_58)] max-sm:w-7"
+              style="z-index: {devs.length - index};"
             />
           </div>
         {/each}
